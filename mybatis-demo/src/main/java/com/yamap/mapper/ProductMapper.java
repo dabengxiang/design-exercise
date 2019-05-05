96f0496cf201e17b42fe93c64a282bd7a1ffe4f5
gyc <836732982@qq.com>
2019-05-05 09:24:20 +0800

    删除不是这里的东西。

--- mybatis-demo/src/main/java/com/yamap/mapper/ProductMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yamap.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.yamap.model.Product;
-
-public interface ProductMapper extends BaseMapper<Product> {
-}
